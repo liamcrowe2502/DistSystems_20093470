eb6ec05be486f8fe4650753357cac70269c0e314
Liam Crowe <20093470@mail.wit.ie>
2024-03-04 10:06:16 +0000

    Initial commit

---
+++ test/dist_systems_20093470.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DistSystems20093470 from '../lib/dist_systems_20093470-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dist_systems_20093470-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DistSystems20093470.DistSystems20093470Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
