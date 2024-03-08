6e399d100a67059023af3506a6d90c7050eab5d0
Liam Crowe <20093470@mail.wit.ie>
2024-03-08 11:07:07 +0000

    Error in postman, cannot get or push

--- lib/dist_systems_20093470-stack.ts
+++ lib/dist_systems_20093470-stack.ts
@@ -70,10 +70,23 @@ export class DistSystems20093470Stack extends cdk.Stack {
             resources: [moviesTable.tableArn],
           }),
         });
+
+        const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+          architecture: lambda.Architecture.ARM_64,
+          runtime: lambda.Runtime.NODEJS_16_X,
+          entry: `${__dirname}/../lambdas/addMovie.ts`,
+          timeout: cdk.Duration.seconds(10),
+          memorySize: 128,
+          environment: {
+            TABLE_NAME: moviesTable.tableName,
+            REGION: "eu-west-1",
+          },
+        });
         
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
+        moviesTable.grantReadWriteData(newMovieFn)
         
         const api = new apig.RestApi(this, "RestAPI", {
           description: "demo api",
